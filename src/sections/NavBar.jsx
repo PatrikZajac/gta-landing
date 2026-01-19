7f9f5cd3af24817879a7f77ab0f9773a5c6fee2f
Patrik Zajac <patrik.zajac145@gmail.com>
2026-01-19 22:30:11 +0100

    feat: hero section

---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <img src="/images/nav-logo.svg" alt="nav-logo" className="scale-90" />
+      <img src="/images/menu.svg" alt="" className="w-10" />
+    </nav>
+  );
+};
+
+export default NavBar;
